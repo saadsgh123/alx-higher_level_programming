95479aa5502fad46717494bb92045bba2994658f
saadsgh123 <sdsghouri@gmail.com>
2023-11-28 16:08:25 +0000

    task 1 try 4 -> forget "and"

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,6 +13,6 @@ else:
 if newLast > 5 or newLast == 5:
     print(f'Last digit of {number} is {newLast} and is greater than 5')
 elif newLast < 6 and newLast != 0:
-    print(f'Last digit of {number} is {newLast} is less than 6 and not 0')
+    print(f'Last digit of {number} is {newLast} and is less than 6 and not 0')
 elif newLast == 0:
     print("Last digit of {0} is {1} and is 0".format(number, last))
