76d3c4aadbfc7539a3246636a31ff11800a16086
saadsgh123 <sdsghouri@gmail.com>
2023-11-28 16:28:22 +0000

    task 4 try 2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('A'), ord('Z') + 1):
-    print("{}".format(chr(i)), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'r' or chr(i) == 'q':
+        print("{}".format(chr(i)), end="")
