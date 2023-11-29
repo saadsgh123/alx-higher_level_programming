0089783745c7cc4b002a58a5025a81665f853095
saadsgh123 <sdsghouri@gmail.com>
2023-11-29 18:13:14 +0000

    task 9 try 1

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(s):
+    new_str = ""
+    for char in s:
+        if 97 <= ord(char) <= 122:
+            new_str += chr(ord(char) - 32)
+        elif char == " ":
+            new_str += char
+        else:
+            new_str += char
+    return new_str
