96028ab57f6837810a833f9ae01494e19e2354fb
saadsgh123 <sdsghouri@gmail.com>
2023-11-29 20:19:58 +0000

    task 13 try 1

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    new = ""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            new += "FizzBuzz "
+        elif i % 3 == 0:
+            new += "Fizz "
+        elif i % 5 == 0:
+            new += "Buzz "
+        else:
+            new += "{} ".format(i)
+    print(new)
