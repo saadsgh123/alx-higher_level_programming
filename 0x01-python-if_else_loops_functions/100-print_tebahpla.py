8b5c9001b720808afa5b3ef0c0cd98c3adcaa4d3
saadsgh123 <sdsghouri@gmail.com>
2023-11-29 21:49:21 +0000

    task 100 try 1

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    new = ""
+    for char in range(ord('z'), ord('a') - 1, -1):
+        if char % 2 == 0:
+            new += "{}".format(chr(char))
+        else:
+            char = char - 32
+            new += "{}".format(chr(char))
+    print("{}".format(new), end="")
