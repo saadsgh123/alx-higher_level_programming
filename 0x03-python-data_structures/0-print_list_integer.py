f3601247e6a8bc58fa6f520ed1de65fb19d0d79f
saadsgh123 <sdsghouri@gmail.com>
2023-12-03 13:57:24 +0000

    task 0 try 1

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        print("The list is empty bro!!")
+    else:
+        for element in my_list:
+            print(element)
