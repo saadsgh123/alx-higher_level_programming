26a03ff7a9f6a93e991801712f662ce5e03624b6
saadsgh123 <sdsghouri@gmail.com>
2023-12-04 14:31:38 +0000

    taks 8 try 4

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence != "" or sentence is not None:
+    if sentence != "":
         result = (len(sentence), sentence[0])
         return result
     return 0, None
