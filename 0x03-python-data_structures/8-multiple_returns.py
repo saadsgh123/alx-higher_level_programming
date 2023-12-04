eb62a97c87cebc33793af76dd27294006e63c184
saadsgh123 <sdsghouri@gmail.com>
2023-12-04 13:54:39 +0000

    task 8 try 3

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,4 @@ def multiple_returns(sentence):
     if sentence != "" or sentence is not None:
         result = (len(sentence), sentence[0])
         return result
-    else:
-        return len(sentence), None
+    return 0, None
