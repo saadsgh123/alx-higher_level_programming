71dd4d10899ec3edd4c46aaaf7d5269650dd328f
saadsgh123 <sdsghouri@gmail.com>
2024-01-04 16:36:14 +0100

    task 3 try 2 (add Zero checker in perimeter method)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,4 +49,6 @@ class Rectangle:
 
     def perimeter(self):
         """Return the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (2 * self.__height) * (2 * self.__width)
