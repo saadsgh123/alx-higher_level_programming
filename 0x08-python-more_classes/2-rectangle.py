357fb12aa15f8a6656298bc3a0a8dd738cd9d091
saadsgh123 <sdsghouri@gmail.com>
2024-01-04 16:38:13 +0100

    task 3 try 3 (replace "*" with "+" in perimeter method)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -51,4 +51,4 @@ class Rectangle:
         """Return the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (2 * self.__height) * (2 * self.__width)
+        return (2 * self.__height) + (2 * self.__width)
