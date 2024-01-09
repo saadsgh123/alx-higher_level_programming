e95fb9b91bc4e11e2eeeec387e0d010ce871bc6f
saadsgh123 <sdsghouri@gmail.com>
2024-01-09 13:56:51 +0100

    task 0 try 4 (add parentis)

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+# 0-lookup.py
+# saadsgh123 <sdsghouri@gmail.com>
 """Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """function that returns the list of available attributes and methods of an object
-    Args:
-        obj: object to check
-    """
+    """Return a list of an object's available attributes."""
     return (dir(obj))
