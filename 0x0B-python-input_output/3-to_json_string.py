85fffcf67051381e8a48a045ca289d4631ab5fd3
saadsgh123 <sdsghouri@gmail.com>
2024-01-10 22:28:40 +0100

    task 3 try 1

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 2-to_json_string.py
+# saadsgh123 <sdsghouri@gmail.com>
+"""Defines a string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    return json.dumps(my_obj)
