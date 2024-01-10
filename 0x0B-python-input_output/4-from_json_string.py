8e3e316eb94423b1b5147bddddab371625aaa6c1
saadsgh123 <sdsghouri@gmail.com>
2024-01-10 22:36:01 +0100

    task 4 try 1

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 4-from_json_string.py
+# saadsgh123 <sdsghouri@gmail.com>
+"""Define json decoder"""
+import json
+
+
+def from_json_string(my_str):
+    """Write a function that returns an object represented by a JSON string"""
+    return json.loads(my_str)
