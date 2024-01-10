1c965d8ef624bc0ee352e5a69991e22bc32d3608
saadsgh123 <sdsghouri@gmail.com>
2024-01-11 00:09:29 +0100

    task 6 try 1

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 7-save_to_json_file.py
+# saadsgh123 <sdsghouri@gmail.com>
+"""Defines a JSON file-reading function."""
+import json
+
+
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
+    with open(filename) as f:
+        json.load(f)
