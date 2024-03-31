9ec7dc3c16ba2a16ac638bb7e3a51019ef8c68fa
saadsgh123 <sdsghouri@gmail.com>
2024-03-31 22:06:40 +0000

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const argument = process.argv;
-if (argument[2] == null) {
-  console.log('No argument');
-} else {
-  console.log(argument[2]);
-}
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-const argument = process.argv;
-console.log(argument[2] + ' is ' + argument[3]);
