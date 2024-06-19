1bdcf42f3c83ad8bfc9c33c87ad0357e3bde2613
saadsgh123 <sdsghouri@gmail.com>
2024-06-19 15:19:36 +0100

    task 5 try 3

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,6 +4,5 @@ if (isNaN(args[0]) || args.length === 0) {
   console.log('Not a number');
 } else {
   const sentence = parseInt(args[0]);
-  console.log("My Number: ", sentence);
+  console.log('My Number: ', sentence);
 }
-
