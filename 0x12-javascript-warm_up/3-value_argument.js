5a68f9bd67e6083a3a5df105361c068520ba2b91
saadsgh123 <sdsghouri@gmail.com>
2024-06-19 12:29:36 +0000

    task 3 try 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-let count = 0;
 if (args[0] === undefined) {
-  console.log('No argument');
+  console.log("No argument");
 } else {
-  while (args[count] !== undefined) {
-    console.log(args[count]);
-    count++;
-  }
+  console.log(args[0]);
 }
