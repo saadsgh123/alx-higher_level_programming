901fdf5665037c441eb16f856b320a249303def8
saadsgh123 <sdsghouri@gmail.com>
2024-06-20 00:41:40 +0100

    task 11 try 5

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const numbers = process.argv.slice(2);
-const sortedNumbers = numbers.sort();
-if (sortedNumbers.length <= 1) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  console.log(sortedNumbers[numbers.length - 2]);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
