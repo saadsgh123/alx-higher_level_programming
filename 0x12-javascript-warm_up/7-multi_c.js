79338ccf86e94e0d88375e6ff376f1d8f7f7be0b
saadsgh123 <sdsghouri@gmail.com>
2024-06-19 16:05:41 +0100

    task 7 try

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < process.argv[2]; i++) {
+  for (let i = 0; i < NUmber(process.argv[2]); i++) {
     console.log('C is fun');
   }
 }
