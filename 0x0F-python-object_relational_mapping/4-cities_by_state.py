ed3aade8c63ac38c00b80ff260eb97a6cd9a97a1
saadsgh123 <sdsghouri@gmail.com>
2024-06-23 21:51:03 +0100

    task 4 try 1

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,12 +4,12 @@ import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
     cur = db.cursor()
-    query = """SELECT * FROM states where name LIKE %s"""
-    cur.execute(query, (sys.argv[4],))
+    cur.execute("SELECT * FROM cities order by id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
