4dcae4550be512e616b24522ffc0af22809fd959
saadsgh123 <sdsghouri@gmail.com>
2024-06-26 14:05:35 +0000

    task 4 try 1

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities order by id ASC")
+    query = "SELECT cities.id, cities.name, states.name FROM cities"
+    join_condition = "LEFT JOIN states ON cities.state_id = states.id"
+    cur.execute("{} {}".format(query, join_condition))
     rows = cur.fetchall()
     for row in rows:
         print(row)
