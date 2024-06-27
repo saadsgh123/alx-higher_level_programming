849bb02ae474ebd9a85f4e959187bc684d7a1036
saadsgh123 <sdsghouri@gmail.com>
2024-06-27 21:34:19 +0100

    task 12 try 6

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -2,9 +2,9 @@
 """ prints the State object with the name passed as argument from the database
 """
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
+from model_state import Base, State
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
