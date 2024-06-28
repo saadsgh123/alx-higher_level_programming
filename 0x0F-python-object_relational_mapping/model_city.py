c13dabae2fc8b340886eb4f4d905093995bc2608
saadsgh123 <sdsghouri@gmail.com>
2024-06-28 18:18:10 +0100

    task 14 try 1

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,23 +2,19 @@
 """
 Contains State class and Base, an instance of declarative_base()
 """
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
-mymetadeeata = MetaData()
-Base = declarative_base(metadata=mymetadeeata)
+from model_state import Base, State
 
 
-class State(Base):
+class City(Base):
     """
     Class with id and name attributes of each state
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, unique=True, autoincrement=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-    # city = relationship("City", back_populates="state")
-
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
