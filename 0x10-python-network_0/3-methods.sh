2bc3226b97a24d23d8e45ee35a405b8eb916c255
saadsgh123 <sdsghouri@gmail.com>
2024-07-05 23:23:49 +0100

    task 3 try 2

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI $1 | grep -i Allow
+# Display all HTTP methods the server of a given URL will accept.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
