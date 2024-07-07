626da58993fe8ba6a5dd9e09748e58dfdbb9e5a8
saadsgh123 <sdsghouri@gmail.com>
2024-07-07 22:48:53 +0100

    task 2 try 3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,12 +9,13 @@ import urllib.request
 import urllib.parse
 
 
-url = sys.argv[1]
-values = {'email': sys.argv[2]}
+if __name__ == '__main__':
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
 
-data = urllib.parse.urlencode(values)
-data = data.encode('utf-8')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
-    print(the_page.decode('utf-8'))
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
