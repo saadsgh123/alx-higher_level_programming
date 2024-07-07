c4dd3152f9ca034deb22a9f590623757b1e5725b
saadsgh123 <sdsghouri@gmail.com>
2024-07-07 22:41:04 +0100

    task 2 try 1

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+url = sys.argv[1]
+values = {'email': sys.argv[2]}
+
+data = urllib.parse.urlencode(values)
+data = data.encode('utf-8')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    the_page = response.read().decode('utf-8')
