f736773e1f4794da2643c2586eba292714dcb2d8
saadsgh123 <sdsghouri@gmail.com>
2024-07-10 00:42:00 +0100

    task 5 try 1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    r = requests.get(sys.argv[1])
+    print(dict(r.headers.items())['x-request-id'])
