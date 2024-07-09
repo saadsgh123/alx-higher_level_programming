814a8288ae4dcb4fc7930a651a18900b8c4c7549
saadsgh123 <sdsghouri@gmail.com>
2024-07-09 23:50:45 +0100

    task 4 try 1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,7 @@
+import requests
+
+if __name__ == '__main__':
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: ", type(r.text))
+    print("\t- content: ", r.text)
