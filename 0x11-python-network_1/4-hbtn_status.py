b60641f6847eb04ed958d35af269c6f6691d0d90
saadsgh123 <sdsghouri@gmail.com>
2024-07-09 23:06:50 +0000

    task 4 try 3 (space)

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,9 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
+
 if __name__ == '__main__':
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: ", type(r.text))
-    print("\t- content: ", r.text)
+    print("\t- type:", type(r.text))
+    print("\t- content:", r.text)
