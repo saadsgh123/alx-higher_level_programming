97ca644083e5c87c4f3953633fc59636fa278d4a
saadsgh123 <sdsghouri@gmail.com>
2024-07-10 01:06:11 +0100

    task 8 try 2

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,7 +10,7 @@ import requests
 
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    payl = {"q": letter}
 
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
