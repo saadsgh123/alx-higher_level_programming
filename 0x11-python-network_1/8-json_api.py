45976c3c60c7e79bdeb2da0e6575718dc0329586
saadsgh123 <sdsghouri@gmail.com>
2024-07-10 01:18:09 +0100

    task 8 try 3

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payl = {"q": letter}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payl)
     try:
         response = r.json()
         if response == {}:
