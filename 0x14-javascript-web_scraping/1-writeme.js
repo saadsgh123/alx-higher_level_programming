46c4aaa880c63c1966122b67e20ab205e0a182b6
saadsgh123 <sdsghouri@gmail.com>
2024-07-30 21:51:04 +0100

    task 1 try 2 🥸

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const filename = process.argv[2];
 const content = process.argv[3];
 
 try {
-  fs.writeFileSync(filename, content, "utf-8");
-} catch(err) {
+  fs.writeFileSync(filename, content, 'utf-8');
+} catch (err) {
   console.error(err);
 }
