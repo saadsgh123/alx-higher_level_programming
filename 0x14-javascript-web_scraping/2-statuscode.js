5969fa99781af01488182f2be99ae76f6dc278e2
saadsgh123 <sdsghouri@gmail.com>
2024-07-30 23:06:04 +0100

    done ✅

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request(url, (err, response) =>  {
+request(url, (err, response) => {
   if (err) console.log(err);
   console.log('code:', response.statusCode);
 });
