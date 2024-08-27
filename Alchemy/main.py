6678b7ffc5ccf2a7dc62da2e383251a37894e7b9
saadsgh123 <sdsghouri@gmail.com>
2024-08-27 20:36:13 +0100

    dmoedme

--- Alchemy/main.py
+++ Alchemy/main.py
@@ -1,1 +1,16 @@
-d
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    from sqlalchemy import create_engine, MetaData, text
+    from User import User, Base
+
+    metadata = MetaData()
+    engine = create_engine("mysql+mysqldb://root:anasaad@localhost/dt_db")
+    Base.metadata.reflect(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_user = User(username="saad", email="<EMAIL>")
+    session.add(new_user)
+    session.commit()
