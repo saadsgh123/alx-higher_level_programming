fd397e9d2336e2e18db59e5acb3c9ad3c9c4497f
saadsgh123 <sdsghouri@gmail.com>
2024-09-01 16:09:32 +0100

    jjjj

--- blueauth/config.py
+++ blueauth/config.py
@@ -4,5 +4,5 @@ import os
 class Config:
     SECRET_KEY = "os.environ.get('SECRET_KEY') or a_secret_key"
     SQLALCHEMY_DATABASE_URI = ('mysql+mysqldb://{}:{}@{}/{}'
-                               .format("root", "anasaad", "localhost", "auth"))
+                               .format("root", "anasaad", "localhost", "authan"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
