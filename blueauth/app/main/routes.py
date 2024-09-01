279af3a62aa4a757aa185201258f93397ce23996
saadsgh123 <sdsghouri@gmail.com>
2024-09-01 15:13:29 +0100

    jjjj

--- blueauth/app/main/routes.py
+++ blueauth/app/main/routes.py
@@ -2,9 +2,11 @@ from flask import render_template
 from flask_login import login_required
 from app.main import main
 
+
 @main.route('/')
 def home():
     return render_template('home.html')
+
 
 @main.route('/dashboard')
 @login_required
